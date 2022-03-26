104ac8e21223961338569a8d6b7075d0d05f82f7
Vishal Thakur <thakurvishal9271@gmail.com>
2022-03-26 12:16:54 +0530

    "my commit"

---
+++ src/main/java/devops/lab2/MyClass.java
@@ -0,0 +1,8 @@
+package devops.lab2;
+
+public class MyClass {
+    public int sum(int a, int b)
+    {
+    	return(a+b);
+    }
+}
---
+++ src/test/java/devops/lab2/MyTestClass.java
@@ -0,0 +1,15 @@
+package devops.lab2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTestClass {
+
+	@Test
+	public void test() {
+		MyClass ob = new MyClass);
+		assertEquals(10,ob.sum(4, 6));
+	}
+
+}
